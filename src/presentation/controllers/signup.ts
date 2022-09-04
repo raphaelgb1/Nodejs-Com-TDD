b4d1ee3b9a15afbc0405818a9ccb10376f457382
Raphael Gonzaga <raphaelgb20@gmail.com>
2022-09-03 21:14:04 -0300

    chore: refactor do signup

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,12 +4,11 @@ import { HttpRequest } from '../protocols/http'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): any {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'))
-    }
-
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'))
+    const requiredFields = ['name', 'email']
+    for (const element of requiredFields) {
+      if (!httpRequest.body[element]) {
+        return badRequest(new MissingParamError(element))
+      }
     }
   }
 }
