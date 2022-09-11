166f002b679b524523bb79d039d13773e22b352c
Raphael Gonzaga <raphaelgb20@gmail.com>
2022-09-10 22:31:32 -0300

    feat: adding EmailValidator

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -94,4 +94,18 @@ describe('SignUp Controller', () => {
       expect(httpResponse.statusCode).toBe(400)
       expect(httpResponse.body).toEqual(new InvalidParamError('email'))
     })
+    test('Should call EmailValidator with correct email', () => {
+      const { sut, emailValidatorStub } = MakeSut()
+      const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+      const httpRequest = {
+        body: {
+          name: 'any_name',
+          email: 'invalid_email@gmail.com',
+          password: 'any_password',
+          passwordConfirmation: 'any_password'
+        }
+      }
+      sut.handle(httpRequest)
+      expect(isValidSpy).toHaveBeenCalledWith('invalid_email@gmail.com')
+    })
 })
