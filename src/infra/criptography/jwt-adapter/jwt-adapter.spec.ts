9e041b64d6690bd8ef2ea51e5ba19351cc36bb76
Raphael Gonzaga <raphaelgb20@gmail.com>
2023-01-23 16:35:11 -0300

    refactor jwt adapter

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -8,21 +8,14 @@ jest.mock('jsonwebtoken', () => ({
 }))
 
 describe('Jason Web Token Adapter', () => {
-    interface SutTypes {
-        sut: JwtAdapter
-    }
-
-    const makeSut = (): SutTypes => {
-        const sut = new JwtAdapter('secret')
-        return {
-            sut
-        }
+    const makeSut = (): JwtAdapter => {
+        return new JwtAdapter('secret')
     }
 
     const makeObj = (): any => ({ id: 'any_id' })
 
     test('Should call sing with correct values', async () => {
-        const { sut } = makeSut()
+        const sut = makeSut()
         const signSpy = jest.spyOn(jwt, 'sign')
         const data = makeObj()
         await sut.encrypt(data.id)
@@ -30,14 +23,14 @@ describe('Jason Web Token Adapter', () => {
     })
 
     test('Should return a token on sign success', async () => {
-        const { sut } = makeSut()
+        const sut = makeSut()
         const data = makeObj()
         const accessToken = await sut.encrypt(data.id)
         expect(accessToken).toBe('any_token')
     })
 
     test('Should throw if JWT throws', async () => {
-        const { sut } = makeSut()
+        const sut = makeSut()
         jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
             throw new Error()
         })
