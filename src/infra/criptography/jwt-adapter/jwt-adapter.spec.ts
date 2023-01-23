a5e1fde029e09f0554ab20df7c6e97f52f1016cc
Raphael Gonzaga <raphaelgb20@gmail.com>
2023-01-23 16:21:11 -0300

    mock jason web token to return any_token on tests from jwt adapter

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -1,6 +1,12 @@
 import jwt from 'jsonwebtoken'
 import { JwtAdapter } from './jwt-adapter'
 
+jest.mock('jsonwebtoken', () => ({
+    sign: async (): Promise<string> => {
+        return await new Promise(resolve => resolve('any_token'))
+    }
+}))
+
 describe('Jason Web Token Adapter', () => {
     interface SutTypes {
         sut: JwtAdapter
@@ -18,7 +24,15 @@ describe('Jason Web Token Adapter', () => {
     test('Should call sing with correct values', async () => {
         const { sut } = makeSut()
         const signSpy = jest.spyOn(jwt, 'sign')
-        await sut.encrypt(makeObj().id)
-        expect(signSpy).toHaveBeenCalledWith(makeObj(), 'secret')
+        const data = makeObj()
+        await sut.encrypt(data.id)
+        expect(signSpy).toHaveBeenCalledWith(data, 'secret')
+    })
+
+    test('Should return a token on sign success', async () => {
+        const { sut } = makeSut()
+        const data = makeObj()
+        const accessToken = await sut.encrypt(data.id)
+        expect(accessToken).toBe('any_token')
     })
 })
