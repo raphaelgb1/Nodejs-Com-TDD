f2189cfa017ae87b0afac1d77c30be3240c08954
Raphael Gonzaga <raphaelgb20@gmail.com>
2023-01-23 10:59:36 -0300

    ensure the bcrypt adapter return false when compare fails

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -49,4 +49,11 @@ describe('Bcrypt Adapter', () => {
         const isValid = await sut.compare('any_value', 'any_hash')
         expect(isValid).toBe(true)
     })
+
+    test('Should return false when compare fails', async () => {
+        const sut = makeSut()
+        jest.spyOn(bcrypt, 'compare').mockReturnValueOnce(await new Promise(resolve => resolve(false as any)))
+        const isValid = await sut.compare('any_value', 'any_hash')
+        expect(isValid).toBe(false)
+    })
 })
