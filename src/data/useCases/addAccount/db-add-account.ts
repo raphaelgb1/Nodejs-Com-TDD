687225877b4990dfa06e5143f721ce8efad1ac5e
Raphael Gonzaga <raphaelgb20@gmail.com>
2023-01-23 22:47:26 -0300

    adjusting

--- src/data/useCases/addAccount/db-add-account.ts
+++ src/data/useCases/addAccount/db-add-account.ts
@@ -2,13 +2,7 @@
 import { AccountModel, AddAccountModel, AddAccount, Hasher, AddAccountRepository } from "./db-add-account-protocols"
 
 export class DBbAddAccount implements AddAccount {
-    private readonly hasher: Hasher
-    private readonly addAccountRepository: AddAccountRepository
-    
-    constructor (Hasher: Hasher, addAccountRepository: AddAccountRepository) {
-        this.hasher = Hasher
-        this.addAccountRepository = addAccountRepository
-    }
+    constructor (private readonly hasher: Hasher, private readonly addAccountRepository: AddAccountRepository) {}
 
     async add (accountData: AddAccountModel): Promise<AccountModel> {
         const hashedPassword = await this.hasher.hash(accountData.password)
