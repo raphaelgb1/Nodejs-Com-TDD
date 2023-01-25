fcb43566cbb8816786def76f56e62db38165f4a4
Raphael Gonzaga <raphaelgb20@gmail.com>
2023-01-25 18:00:15 -0300

    new feature add login factory

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import setUpMiddlewares from './middlewares'
 import setUpRoutes from './routes'
+import * as env from 'dotenv'
 
 const app = express()
+env.config()
 setUpMiddlewares(app)
 setUpRoutes(app)
 export default app
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,6 @@
 export default {
     mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/node-com-tdd',
-    port: process.env.PORT ?? 5050
+    port: process.env.PORT ?? 5050,
+    jwtSecret: process.env.JWT_SECRET ?? 'tj670',
+    salt: process.env.SALT ?? 12
 }
