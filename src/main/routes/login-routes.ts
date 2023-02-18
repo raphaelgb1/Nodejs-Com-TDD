efc3d923cb9d5e9926ef6065c7c8cc8fa479cee7
Raphael Gonzaga <raphaelgb20@gmail.com>
2023-02-18 16:21:58 -0300

    adjusting routes

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,7 +1,8 @@
 import { Router } from "express"
 import { adaptRoute } from "../adapters/express/express-routes-adapter"
 import { makeLoginController } from "../factories/controllers/login/login-controller-factory"
-import { makeSignUpController } from "../factories/signup/signup-controller-factory"
+import { makeSignUpController } from "../factories/controllers/signup/signup-controller-factory"
+
 
 export default async (router: Router): Promise<void> => {
     router.post('/signup', (await adaptRoute(makeSignUpController())))
