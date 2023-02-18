28c1037ebeed8f716287510968591761d731eb52
Raphael Gonzaga <raphaelgb20@gmail.com>
2023-02-18 19:29:49 -0300

    refactor in validation

--- src/infra/validators/email-validator-adapter.ts
+++ src/infra/validators/email-validator-adapter.ts
@@ -1,5 +1,5 @@
-import { EmailValidator } from "../../../presentation/protocols"
 import validator from 'validator'
+import { EmailValidator } from "../../validation/protocols/emailValidator"
 
 export class EmailValidatorAdapter implements EmailValidator {
     isValid (email: string): boolean {
