e528f19a6a2c12fe7dd80848704f40a47e019dad
Raphael Gonzaga <raphaelgb20@gmail.com>
2023-02-18 16:00:12 -0300

    refator, detaching db authentication

---
+++ src/main/factories/use-cases/authentication/db-authentication-factory.ts
@@ -0,0 +1,14 @@
+import { DbAuthentication } from "../../../../data/useCases/authentication/db-authentication"
+import { Authentication } from "../../../../domain/useCases/authentication"
+import { BcryptAdapter } from "../../../../infra/criptography/bcrypter-adapter/bcrypt-adapter"
+import { JwtAdapter } from "../../../../infra/criptography/jwt-adapter/jwt-adapter"
+import { AccountMongoRepository } from "../../../../infra/db/mongodb/account/account-mongo-repository"
+import env from "../../../config/env"
+
+export const makeDbAuthentication = (): Authentication => {
+    const bcryptAdapter = new BcryptAdapter(parseInt(env.salt as any))
+    const jwtAdapter = new JwtAdapter(env.jwtSecret)
+    const accountMongoRepository = new AccountMongoRepository()
+    const dbAuthentication = new DbAuthentication(accountMongoRepository, bcryptAdapter, jwtAdapter, accountMongoRepository)
+    return dbAuthentication
+}
