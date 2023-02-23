d18ceab43fbf71d486e70198512a236d733b270e
Raphael Gonzaga <raphaelgb20@gmail.com>
2023-02-22 23:03:26 -0300

    addjusting

--- src/data/useCases/load-account-by-token/db-load-account-by-token.spec.ts
+++ src/data/useCases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -81,4 +81,13 @@ describe('Db Load Account by Token UseCase', () => {
         const httpResponse = await sut.load('any_token', role)
         expect(httpResponse).toEqual(makeFakeAccount())
     })
+
+    // test('Should return null if Load by Account by Token Repository returns null', async () => {
+    //     const { sut, decrypterStub } = makeSut()
+    //     jest.spyOn(decrypterStub, 'decrypt').mockImplementationOnce(async () => {
+    //         return await Promise.reject(new Error())
+    //     })
+    //     const promise = await sut.load('any_token', role)
+    //     await expect(promise).rejects.toThrow()
+    // })
 })
