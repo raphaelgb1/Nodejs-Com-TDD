24ae0e7c4a2de3f41ba6ecd11a156dfaec84f7b7
Raphael Gonzaga <raphaelgb20@gmail.com>
2023-02-23 20:38:42 -0300

    refactor

--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express"
 import { adaptRoute } from "../adapters/express/express-routes-adapter"
-import { makeAddSurveyController } from "../factories/controllers/add-survey/add-survey-controller-factory"
+import { makeAddSurveyController } from "../factories/controllers/survey/add-survey/add-survey-controller-factory"
 
 export default async (router: Router): Promise<void> => {
     router.post('/surveys', (await adaptRoute(makeAddSurveyController())))
