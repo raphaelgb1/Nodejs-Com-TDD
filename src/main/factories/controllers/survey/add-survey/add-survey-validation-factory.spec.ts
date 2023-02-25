09ab9a1894b6c32f953b2612d26a82cc54ed5e86
Raphael Gonzaga <raphaelgb20@gmail.com>
2023-02-25 14:51:51 -0300

    refactor

--- src/main/factories/controllers/survey/add-survey/add-survey-validation-factory.spec.ts
+++ src/main/factories/controllers/survey/add-survey/add-survey-validation-factory.spec.ts
@@ -1,8 +1,8 @@
-import { Validation } from "../../../../presentation/protocols/validation"
-import { RequiredFieldValidation, ValidationComposite } from "../../../../validation/validators"
+import { Validation } from "../../../../../presentation/protocols/validation"
+import { RequiredFieldValidation, ValidationComposite } from "../../../../../validation/validators"
 import { makeAddSurveyValidation } from "./add-survey-validation-factory"
 
-jest.mock("../../../../validation/validators/validation-composite")
+jest.mock("../../../../../validation/validators/validation-composite")
 
 describe('Add Survey Validation Factory', () => {
     test('Should call Validation Composite with all validations', () => {
