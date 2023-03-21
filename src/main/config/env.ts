0fa1ec748dd83013a79765a865730903c91d154c
Raphael Gonzaga <raphaelgb20@gmail.com>
2023-03-21 15:07:06 -0300

    fix: fixing the map error on dist folder

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-    mongoUrl: process.env.MONGO_URL ?? 'mongodb://mongo:27017/node-com-tdd',
+    mongoUrl: process.env.MONGO_URL ?? 'mongodb://0.0.0.0:27017/node-com-tdd',
     port: process.env.PORT ?? 5050,
     jwtSecret: process.env.JWT_SECRET ?? 'tj670',
     salt: process.env.SALT ?? 12
