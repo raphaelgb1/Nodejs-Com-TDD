1d463f991f5dcd8fb0970bfd937e15d175f9bf29
Raphael Gonzaga <raphaelgb20@gmail.com>
2023-03-21 15:30:50 -0300

    refactor of interfaces on domain models

--- src/domain/useCases/add-survey.ts
+++ src/domain/useCases/add-survey.ts
@@ -1,10 +1,6 @@
-import { SurveyAnswerModel } from "../models/survey-model"
+import { SurveyModel } from "../models/survey-model"
 
-export type AddSurveyModel = {
-    question: string
-    answers: SurveyAnswerModel[]
-    date: Date
-}
+export type AddSurveyModel = Omit<SurveyModel, 'id'>
 export interface AddSurvey {
     add (data: AddSurveyModel): Promise<void>
 }
