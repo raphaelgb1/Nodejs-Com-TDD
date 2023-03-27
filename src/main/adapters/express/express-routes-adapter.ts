e98cca8ed3d2e43da0bd55c834e09626782d45dc
Raphael Gonzaga <raphaelgb20@gmail.com>
2023-03-27 11:28:57 -0300

    feat: ensure save survey result routes return 200 on success

--- src/main/adapters/express/express-routes-adapter.ts
+++ src/main/adapters/express/express-routes-adapter.ts
@@ -1,10 +1,12 @@
-import { Request, Response } from "express"
 import { Controller, HttpRequest } from "@/presentation/protocols"
+import { Request, Response } from "express"
 
 export const adaptRoute = async (controller: Controller): Promise<any> => {
     const anyFunction = async (req: Request, res: Response): Promise<void> => {
         const httpRequest: HttpRequest = {
-            body: req.body
+            body: req.body,
+            params: req.params,
+            accountId: req.accountId
         }
         const httpResponse = await controller.handle(httpRequest)
         if ([200, 204].includes(httpResponse.statusCode)) {
--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -1,8 +1,10 @@
 import { AddSurveyModel } from '@/domain/useCases/survey/add-survey'
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongodb-helper'
 import app from "@/main/config/app"
+import { sign } from 'jsonwebtoken'
 import { Collection } from 'mongodb'
 import request from 'supertest'
+import env from '../config/env'
 
 let surveyCollection: Collection
 let accountCollection: Collection
@@ -19,18 +21,17 @@ const makeFakeSurvey = (): AddSurveyModel => ({
     date: new Date()
 })
 
-// const makeAccessToken = async (): Promise<string> => {
-//     const result = await accountCollection.insertOne({
-//         name: 'any_name',
-//         email: 'any_email@gmail.com',
-//         password: 'any_password',
-//         role: 'admin'
-//     })
-//     const id = result.insertedId
-//     const accessToken = sign({ id }, env.jwtSecret)
-//     await accountCollection.updateOne({ _id: id }, { $set: { accessToken } })
-//     return accessToken
-// }
+const makeAccessToken = async (): Promise<string> => {
+    const result = await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@gmail.com',
+        password: 'any_password'
+    })
+    const id = result.insertedId
+    const accessToken = sign({ id }, env.jwtSecret)
+    await accountCollection.updateOne({ _id: id }, { $set: { accessToken } })
+    return accessToken
+}
 describe('Survey Routes', () => {
     beforeAll(async () => {
         await MongoHelper.open(String(process.env.MONGO_URL))
@@ -51,6 +52,16 @@ describe('Survey Routes', () => {
         test('Should return 403 on SaveSurveyResult without access token', async () => {
             await request(app)
                 .put('/api/surveys/any_id/results')
+                .send(makeFakeSurvey().answers[0].answer)
+                .expect(403)
+        })
+
+        test('Should return 200 on SaveSurveyResult with access token', async () => {
+            const accessToken = await makeAccessToken()
+            const result = await surveyCollection.insertOne(makeFakeSurvey())
+            await request(app)
+                .put(`/api/surveys/${result.insertedId.toString()}/results`)
+                .set('x-access-token', accessToken)
                 .send(makeFakeSurvey().answers[0].answer)
                 .expect(403)
         })
